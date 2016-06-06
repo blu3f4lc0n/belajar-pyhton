1f6265316e19b65a391b3b0cf5fb6cd4f674e97a
Yudha Satya Ami Putra <f4lc0n3r@gmail.com>
2016-06-07 02:23:41 +0700

    Add files via upload

---
+++ halo.py
@@ -0,0 +1,11 @@
+# program 1
+
+print('Halo Dunia')
+print('koyok biasane jenenge sopo')
+Name = input()
+print('Halo beh '+ Name)
+print('Dowone jenege Rek')
+print(len(Name))
+print('Umurmu pira ?')
+Umur = input()
+print('Umurmu saiki ' +str(int(Umur)+1)+ ' lho tahun ngarep')
